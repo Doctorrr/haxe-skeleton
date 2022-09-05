4f0be4ffcbec9c51aa7b307cb9f5b679daab1f6b
Alexey Babak <regs@babak.ru>
2022-09-05 18:27:18 +0300

    Update Main.hx

--- src/Main.hx
+++ src/Main.hx
@@ -1,17 +1,39 @@
 /**
  * Класс Main
  */
-class Main {
-
-    /**
-    * Функция main
-    */
+ class Main {
+    // Haxe applications have a static entry point called main
     static function main() {
-        var listOfInts = new List<Int>();
-        for(i in 1...11) listOfInts.add(i);
-        var listOfEvenInts = listOfInts.filter(function (e) return e % 2 == 0);
-        var withSeparator : String = listOfEvenInts.join(" / ");
-
-        Sys.println( withSeparator + " Hello team!");
+      // Anonymous structures.
+      var playerA = { name: "Simon", move: Paper }
+      var playerB = { name: "Nicolas", move: Rock }
+          
+      // Array pattern matching. A switch can return a value.
+      var result = switch [playerA.move, playerB.move] {
+        case [Rock, Scissors] | 
+             [Paper, Rock] |
+             [Scissors, Paper]: Winner(playerA);
+              
+        case [Rock, Paper] |
+             [Paper, Scissors] |
+             [Scissors, Rock]: Winner(playerB);
+              
+        case _: Draw;
+      }
+      // Paper vs Rock, who wins?
+      trace('result: $result');
     }
-}
+  }
+              
+  // Allow anonymous structure named as type.
+  typedef Player = { name: String, move: Move }
+  
+  // Define multiple enum values.
+  enum Move { Rock; Paper; Scissors; }
+  
+  // Enums in Haxe are algebraic data type (ADT), so they can hold data.
+  enum Result { 
+    Winner(player:Player); 
+    Draw; 
+  }
+
